fcf6faa959d470e572ae79952c4b4f5499cfb72b
Linus <linus@wieland.lu>
2022-12-29 15:07:58 +0100

    mailing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'netlify-mail';
+
+  ngOnInit(): void {
+    const data = {
+      subscriberName: 'lini',
+      subscriberEmail: 'vitocorleone77@gmail.com',
+    };
+    //call to the Netlify Function you created
+    fetch('./.netlify/functions/triggerSubscribeEmail', {
+      method: 'POST',
+      body: JSON.stringify({
+        subscriberName: data.subscriberName,
+        subscriberEmail: data.subscriberEmail,
+        inviteeEmail: 'anmeldungen@schlosswochen.ch',
+      }),
+    });
+  }
 }
